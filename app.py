3a3d9a3c677a896b5960d7603720cdf5ba438cce
alkolhar <alexkoller90@gmail.com>
2022-03-16 09:43:27 +0100

    added review files

---
+++ app.py
@@ -0,0 +1,21 @@
+from dash import Dash, html
+import pandas as pd
+
+df = pd.read_xml('./data/kitchen_&_housewares/unlabeled.review')
+
+
+def generate_table(dataframe, max_rows=10):
+    return html.Table([html.Thead(html.Tr([html.Th(col) for col in dataframe.columns])), html.Tbody(
+        [html.Tr([html.Td(dataframe.iloc[i][col]) for col in dataframe.columns]) for i in
+         range(min(len(dataframe), max_rows))])])
+
+
+app = Dash(__name__)
+
+app.layout = html.Div([
+    html.H1(children="hallo"),
+    generate_table(df)
+])
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
